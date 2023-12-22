da122cf85dcba9831e983a7ebf7405227d046dac
Justin Adams <justinadams7991@gmail.com>
2023-12-22 11:00:29 -0500

    feat: Initialise app and data source config
    - add Spring Initializr code (with Web, JPA, Thymeleaf dependencies)
    - add dependencies to handle SQLite dialect and driver
    - add Spring JPA props to `application.properties`

---
+++ src/main/java/com/jradams/melophobia/MelophobiaApplication.java
@@ -0,0 +1,13 @@
+package com.jradams.melophobia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelophobiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelophobiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jradams/melophobia/MelophobiaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jradams.melophobia;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelophobiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
