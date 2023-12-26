5b1a47f6e22d64a3abca20cb967b9f8a6a032b15
Justin Adams <justinadams7991@gmail.com>
2023-12-25 20:24:55 -0500

    feat: Create artist detail mapping
    - add mapping to display details of a specifc artist (inc. releases)
    - add `@ManyToMany` mapping in Artist entity to display associated
      releases
    - add hyperlink for artist names in artist list template that links
      to details page
    - add template for artist details (placeholder image used for
      releases)
    - remove unneeded imports

--- src/main/java/com/jradams/melophobia/controller/ArtistController.java
+++ src/main/java/com/jradams/melophobia/controller/ArtistController.java
@@ -1,11 +1,17 @@
 package com.jradams.melophobia.controller;
 
+import com.jradams.melophobia.entity.Artist;
+import com.jradams.melophobia.entity.Release;
 import com.jradams.melophobia.repository.ArtistRepository;
+import com.jradams.melophobia.repository.ReleaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/artist")
@@ -24,4 +30,15 @@ public class ArtistController {
 
         return "artist/index";
     }
+
+    @GetMapping("/{id}")
+    public String showArtistDetail(@PathVariable(value = "id") long id, Model model) {
+        Optional<Artist> artist = artistRepository.findById(id);
+
+        artist.ifPresent(v -> model.addAttribute("artist", v));
+
+        return "artist/detail";
+    }
+
+
 }
--- src/main/java/com/jradams/melophobia/entity/Artist.java
+++ src/main/java/com/jradams/melophobia/entity/Artist.java
@@ -2,6 +2,7 @@ package com.jradams.melophobia.entity;
 
 import com.jradams.melophobia.entity.backing.ArtistType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -9,7 +10,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OrderBy;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
@@ -18,10 +18,8 @@ import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.annotations.SortNatural;
 
-import java.util.List;
 import java.util.Set;
 import java.util.SortedSet;
-import java.util.TreeSet;
 
 @Entity
 @Getter
@@ -60,4 +58,7 @@ public class Artist {
             inverseJoinColumns = @JoinColumn(name = "genre_id"))
     @SortNatural
     private SortedSet<Genre> genres;
+
+    @ManyToMany(fetch = FetchType.EAGER, mappedBy = "artists")
+    private Set<Release> releases;
 }
