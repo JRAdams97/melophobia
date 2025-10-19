e861b89be2e539c97cbed6515c37d076c0663752
Justin Adams <justinadams7991@gmail.com>
2025-10-19 11:39:08 -0400

    Rename Melophobia project
    - rename Project to 'Service' to better split backend/frontend (they
      will still exist under the same solution)

---
+++ Service/Program.cs
@@ -0,0 +1,25 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.MapControllers();
+
+app.UseCors(policyBuilder => policyBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+app.Run();
