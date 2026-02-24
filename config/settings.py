b12db87efe9ffe3309d4258e90c0c2c2c5e2fe2d
Justin Adams <justinadams7991@gmail.com>
2026-02-24 18:08:22 -0500

    Create custom auth user
    - add new app named 'user' using `django-admin`
    - add overwrite to standard user in 'user/models'

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import AUTH_USER_MODEL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure-lmrhs3(1k_8x*b8(9q(9b!!k612^k$dnx0^j_)@pre2c%+4o%q
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+AUTH_USER_MODEL = 'user.User'
 
 # Application definition
 
@@ -37,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'melophobia.main'
+    'melophobia.main',
+    'melophobia.user'
 ]
 
 MIDDLEWARE = [
