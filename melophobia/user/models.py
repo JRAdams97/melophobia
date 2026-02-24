e24a320d75da4c939bfbeb2d01d773fefac54102
Justin Adams <justinadams7991@gmail.com>
2026-02-24 18:23:33 -0500

    Create Profile model
    - add new model to user\models to represent a user's profile
    - add migration to create the profile entity

--- melophobia/user/models.py
+++ melophobia/user/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class User(AbstractUser):
     # Custom user model
     pass
+
+class Profile(models.Model):
+    # Profile model associated with an individual User
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
