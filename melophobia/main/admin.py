b40724c93f9c3b908eddb9a3098b58c0632dfb2e
Justin Adams <justinadams7991@gmail.com>
2026-02-24 18:51:42 -0500

    Register models to admin site
    - add newly created models to the admin site for bootstrapping needs

--- melophobia/main/admin.py
+++ melophobia/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from melophobia.main.models import Location, Region, Country
+
+admin.site.register(Country)
+admin.site.register(Region)
+admin.site.register(Location)
